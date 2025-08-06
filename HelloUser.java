f77c6e31b054b7da54f3972b0a70875ddc054bb1
vamsikrishnakonidena379 <vamsikrishnakonidena79@gmail.com>
2025-08-06 12:25:52 -0400

    Update HelloUser.java

--- HelloUser.java
+++ HelloUser.java
@@ -3,7 +3,8 @@ public class HelloUser{
 public static void main(String[] args)
 {
 
- System.out.println("I am learning jenkins day 1");
+ System.out.println("This code is cloned by using Jenkins");
 }
 }
+
 
